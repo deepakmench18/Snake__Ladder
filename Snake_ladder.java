a81b9ea76f2dd2a6140b776e71bb6b2971585ff4
deepakmench18 <deepakmench18@gmail.com>
2021-06-24 09:35:53 +0530

    Initial Position of Player

---
+++ Snake_ladder.java
@@ -0,0 +1,7 @@
+public class UC1_Initial_position {
+	public static void main(String[] args) {
+		System.out.println("Welcome ");
+		int P = 0;
+		System.out.println(P + "Initial position of player is " );
+	}
+}
