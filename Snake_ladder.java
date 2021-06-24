0a437cd6c8df5035a3eef1469c41ea67edf05f97
deepakmench18 <deepakmench18@gmail.com>
2021-06-24 09:51:52 +0530

    Taking Six Random Number

--- Snake_ladder.java
+++ Snake_ladder.java
@@ -3,5 +3,7 @@ public class UC1_Initial_position {
 		System.out.println("Welcome ");
 		int P = 0;
 		System.out.println(P + "Initial position of player is " );
-	}
+                int Dice = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+		System.out.println("Dice roll: " + Dice);
+}
 }
