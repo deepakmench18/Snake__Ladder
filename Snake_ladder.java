23d6b2518677fccc5f03591be4c751ac2415c1fc
deepakmench18 <deepakmench18@gmail.com>
2021-06-24 10:09:32 +0530

    CHECK NO PLAY LADDER

--- Snake_ladder.java
+++ Snake_ladder.java
@@ -2,8 +2,29 @@ public class UC1_Initial_position {
 	public static void main(String[] args) {
 		System.out.println("Welcome ");
 		int P = 0;
+                System.out.print("Enter player name: ");
+		Scanner sc = new Scanner(System.in);
+		String Name = sc.next();
+		System.out.println("Player name is: " + Name);
 		System.out.println(P + "Initial position of player is " );
                 int Dice = (int) (Math.floor(Math.random() * 10) % 6 + 1);
 		System.out.println("Dice roll: " + Dice);
+		int number = (int) (Math.floor(Math.random() * 10) % 3);
+		switch (number) {
+			case 0:
+				System.out.println("No play");
+				P = P + 0;
+				break;
+			case 1:
+				System.out.println("Ladder");
+				P = P + Dice;
+				break;
+			default:
+				System.out.println("Snake");
+				P = P - Dice;
+				if (P < 0)
+					P = 0;
+		}
+		System.out.println("Player position is: " + P);
 }
 }
